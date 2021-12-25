a1a00e94549e6b6203baca2d3ce5ffa945e09147
Bren Dahl <dahl.brendan@gmail.com>
2021-12-25 11:45:21 -0500

    chore: improve debug logging to determine region/brand

--- custom_components/ha_kia_hyundai/api_cloud_util.py
+++ custom_components/ha_kia_hyundai/api_cloud_util.py
@@ -1,17 +1,24 @@
+import logging
 from .api_cloud_us_kia import ApiCloudUsKia
 from .api_cloud_ca_kia import ApiCloudCaKia
 from .api_cloud_ca_hyundai import ApiCloudCaHyundai
 from .api_cloud_us_hyundai import ApiCloudUsHyundai
 from .const import REGION_USA, REGION_CANADA, BRAND_KIA, BRAND_HYUNDAI
 
+_LOGGER = logging.getLogger(__name__)
+
 
 def api_cloud_for_region_and_brand(region: str, brand: str):
     if region == REGION_USA and brand == BRAND_KIA:
+        _LOGGER.debug("US KIA in use")
         return ApiCloudUsKia
     if region == REGION_USA and brand == BRAND_HYUNDAI:
+        _LOGGER.debug("US HYUNDAI in use")
         return ApiCloudUsHyundai
     if region == REGION_CANADA and brand == BRAND_KIA:
+        _LOGGER.debug("CA KIA in use")
         return ApiCloudCaKia
     if region == REGION_CANADA and brand == BRAND_HYUNDAI:
+        _LOGGER.debug("CA HYUNDAI in use")
         return ApiCloudCaHyundai
     return None
